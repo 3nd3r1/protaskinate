9bf7bf6365ff128d664acbb7d94d0470a04d5f97
3nd3r1 <ender.spam.guru@gmail.com>
2024-10-01 10:41:41 +0300

    fix(dashboard): Make index redirect to dashboard

--- protaskinate/routes/dashboard.py
+++ protaskinate/routes/dashboard.py
@@ -1,11 +1,16 @@
 """protaskinate/routes/dashboard.py"""
 
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template, url_for
 from flask_login import login_required
 
 blueprint = Blueprint("dashboard", __name__)
 
-@blueprint.route("/")
+@blueprint.route("/", methods=["GET"])
+@login_required
+def index_route():
+    """Redirect to the dashboard"""
+    return redirect(url_for("dashboard.dashboard_route"))
+
 @blueprint.route("/dashboard", methods=["GET"])
 @login_required
 def dashboard_route():
